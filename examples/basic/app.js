bf676461fccd6756d2ea501ab772ce06face8cc1
Yatharth Khatri <yatharth01@gmail.com>
2017-04-21 22:44:25 +0530

    success color replace and basic example

--- examples/basic/app.js
+++ examples/basic/app.js
@@ -1,7 +1,34 @@
 import Notification from './../../src'
 
-const e = Notification`
-  I am a notification!
+// Declare notifications
+const accountDeletionFail = Notification`
+  -- type: warning
+  -- position: top-left
+  # Account Deactivation
+  Oops, we're facing some issues connecting to our servers
 `
 
-e()
+const accountDeletionSuccess = Notification`
+  -- type: success
+  -- position: top-right
+  # Account Deactivation
+  Your account has been successfully deactivated
+`
+
+const afterAccountDeletion = Notification`
+  -- type: info
+  -- position: bottom-right
+  # Info
+  You'll now be logged out shortly
+`
+
+// Trigger notifications
+accountDeletionFail()
+
+setTimeout(() => {
+  accountDeletionSuccess()
+}, 2000)
+
+setTimeout(() => {
+  afterAccountDeletion()
+}, 4000)
